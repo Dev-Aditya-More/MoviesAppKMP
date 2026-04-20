4ceac24308c603de7adce3d84e14ca8e30569b90
Aditya More <aditya1875more@gmail.com>
2026-04-20 19:57:29 +0530

    bump app version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -71,8 +71,8 @@ android {
         applicationId = "org.example.movieapplibrary"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2"
 
         buildConfigField(
             "String",
